9d3d53216826325fac1f8776189e5d36b7406b11
afreediz <afreedisulfiker@gmail.com>
2024-06-18 13:25:38 +0530

    back to 0 domestic

--- src/Pages/Packages/Domestic.jsx
+++ src/Pages/Packages/Domestic.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import {domestic} from './packagesdata'
 export default function Domestic() {
+  const [expandIndex, setExpandIndex] = useState(-1)
+
   return (
-    <div className='min-h-screen flex items-center justify-center'>Domestic</div>
-  )
+    <div className="container mx-auto px-4 py-8">
+        {domestic && domestic.map((tourPackage, index) => (
+            <div 
+              key={index} 
+              className={`collapse ${expandIndex == index ? "collapse-open" : "collapse-close"}  border rounded-lg shadow-sm cursor-pointer bg-gray-100 hover:bg-gray-200 transition-colors duration-300 mb-8`}
+              onClick={() => setExpandIndex((index === expandIndex) ? -1 : index)}
+              >
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium text-black text-left uppercase flex justify-between">
+                  <div className="">
+                    {tourPackage.package_name}  {tourPackage.duration}
+                  </div>
+                  <div className="">{expandIndex == index ? "-" : "+"}</div>
+                </div>
+                <div className="collapse-content text-left text-black">
+                    {tourPackage.itinerary && tourPackage.itinerary.map((day, dayIndex) => (
+                        <div key={dayIndex} className="border p-4 rounded shadow">
+                            <h3 className="text-xl font-semibold mb-2">{day.day}</h3>
+                            <p className="text-gray-900">{day.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        ))}
+    </div>
+);
 }
