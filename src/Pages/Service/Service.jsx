526b543d582569c9a5d7cdddc582f64c5c46a3a0
afreediz <afreedisulfiker@gmail.com>
2024-06-18 17:58:57 +0530

    services hero

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -11,7 +11,15 @@ function Service() {
       <WhatsappButton/>
         <Navbar/>
         <div className='min-h-screen'>
-
+          <div className="relative min-h-[50vh] bg-cover" style={{ backgroundImage: "url('/Images/service/servicesfirst.avif')" }}>
+            <div className="absolute inset-0 bg-black opacity-30"></div> {/* Dark overlay */}
+            <div className="relative h-full flex flex-col items-center py-20 z-2 text-white p-4"> {/* Content */}
+              <h1 className="text-white text-4xl md:text-5xl lg:text-6xl uppercase font-bold tracking-wide text-center  shadow-sm">WORLDS BEST QUALITY SERVICES</h1>
+              <p className="mt-3 shadow text-white font-bold text-xl text-center max-w-6xl">
+                Experience unparalleled excellence with our top-tier services, dedicated to meeting your needs with precision and care.
+                 We strive to deliver outstanding quality that sets us apart from the rest."</p>
+            </div>
+          </div>
         </div>
         <Footer/>
     </div>
