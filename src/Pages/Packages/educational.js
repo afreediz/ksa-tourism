0e18c62153648c609351f745c7007fc0cf624afb
afreediz <afreedisulfiker@gmail.com>
2024-06-19 16:29:30 +0530

    upto karnataka

---
+++ src/Pages/Packages/educational.js
@@ -0,0 +1,89 @@
+const educational = {
+    educational: [
+      {
+        name: "ONE DAY TRIP",
+        destinations: [
+          "WONDERLA AND LULU MALL",
+          "ATHIRAPALLY,VAZHACHAL,SILVERSTORM",
+          "MUNNAR",
+          "MARAYUR",
+          "VAGAMON",
+          "IDUKKI",
+          "KODAIKANAL",
+          "OOTY",
+          "ALLAPUZHA HOUSE BOAT",
+          "KANYAKUMARI",
+          "THENMALA"
+        ]
+      },
+      {
+        name: "2 DAYS 3 NIGHTS",
+        packages: [
+          { destinations: ["OOTY-WAYANAD"] },
+          { destinations: ["MARAYUR-WONDERLA"] },
+          { destinations: ["VAGAMON-WONDERLA"] },
+          { destinations: ["IDUKKI-WONDERLA"] },
+          { destinations: ["OOTY-WONDERLA"] },
+          { destinations: ["OOTY-MALAMPUZHA"] },
+          { destinations: ["KODAIKANAL-MUNNAR"] },
+          { destinations: ["MARAYUR-MUNNAR"] },
+          { destinations: ["MARAYUR-KODAIKANAL"] },
+          { destinations: ["MUNNAR-WONDERLA"] },
+          { destinations: ["MYSORE-COORG"] },
+          { destinations: ["MYSORE-OOTY"] },
+          { destinations: ["MYSORE-BANGLORE"] },
+          { destinations: ["MYSORE-WONDERLA-BANGLORE"] },
+          { destinations: ["VAGAMON-IDUKKI"] },
+          { destinations: ["MYSORE-CHICKMANGLORE"] },
+          { destinations: ["COORG-CHICKMANGLORE"] },
+          { destinations: ["DANUSHKODI-MADURAI-KODAIKANAL"] },
+          { destinations: ["MYSORE-SRAVANABALAGOLA-BELUR"] }
+        ]
+      },
+      {
+        name: "3DAYS 4 NIGHTS PACKAGE",
+        packages: [
+          { destinations: ["CHICKMANGLORE-BELUR-MYSORE"] },
+          { destinations: ["CHICKMANGLORE-HALIBEED-SARAVANABALAGOLA-MYSORE"] },
+          { destinations: ["MYSORE-CHICKMANGLORE-WONDERLA"] },
+          { destinations: ["WONDERLA-HALIBEED-SARAVANABALAGOLA-MYSORE"] },
+          { destinations: ["MYSORE-BANGLORE-WONDERLA"] },
+          { destinations: ["MYSORE-CHICKMANNGLORE-COORG"] },
+          { destinations: ["MYSORE-COORG-WAYANAD"] },
+          { destinations: ["MYSORE-OOTY-WONDERLA"] },
+          { destinations: ["KODAIKANAL-MUNNAR-WONDERLA"] },
+          { destinations: ["KODAIKANAL-MARAYUR-WONDERLA"] },
+          { destinations: ["WAYANAD-OOTY-WONDERLA"] }
+        ]
+      },
+      {
+        name: "4DAYS 5NIGHTS PACKAGE",
+        packages: [
+          { destinations: ["OOTY-WAYANAD-WONDERLA"] },
+          { destinations: ["KODAIKANAL-MARAYUR-MUNNAR-WONDERLA"] },
+          { destinations: ["VAGAMON-RAMAKALMEDU-IDUKKI-MUNNAR-MARAYUR-WONDERLA"] },
+          { destinations: ["BANGLORE-MYSORE-OOTY-WAYANAD-WONDERLA"] },
+          { destinations: ["COORG-CHICKMANGLORE-BELUR-MYSORE-WONDERLA"] },
+          { destinations: ["HYDERABAD"] },
+          { destinations: ["DANDELI-UDUPI-COORG-CHICKKMANGLORE"] },
+          { destinations: ["HAMPI-BANGLORE-MYSORE-BADAMI-AILHOLE PATADAKAL-BANGLORE/WONDERLA"] },
+          { destinations: ["MYSORE-SARAVANABALAGOLA-HALIBEED-CHICKMANGLORE-COORG"] },
+        ]
+      },
+      {
+        name: "5DAYS 6NIGHTS PACKAGE",
+        packages: [
+          { destinations: ["GOA-COORG-CHICKMANGLORE-UDUPI"] },
+          { destinations: ["HYDERABAD"] },
+          { destinations: ["BANGLORE-MYSORE-COORG-CHICKMANGLORE-BELUR-WONDERLA"] },
+          { destinations: ["HAMPI-BANGLORE-MYSORE-CHICKMANGLORE-UDUPI-WONDERLA"] },
+          { destinations: ["COORG=BELUR-CHICKMANGLORE-DENDELI-UDUPI"] },
+          { destinations: ["WAYANAD-OOTY-MYSORE-CHICKMANGLORE-KUDUK-WONDERLA"] },
+          { destinations: ["MYSORE-SRAVANBALAGOLA-HALIBEED-BELUR-CHICKMANGLORE-COORG-WONDERLA"] },
+          { destinations: ["MYSORE-SRAVANABALAGOLA-HALIBEED-HAMPI-BADAMI AILHOLIE PATADAKAL-UDUPI-STMARY’S ISLAND/WONDERLA"] },
+        ]
+      },
+    ]
+  };
+
+  export default educational;
