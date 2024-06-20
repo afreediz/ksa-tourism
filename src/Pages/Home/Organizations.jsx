e703a82588117a137d38919d4de1f3210646b13d
mirz7 <mirzzpc1289@gmail.com>
2024-06-20 17:02:06 +0530

    contact num changed and org image added

--- src/Pages/Home/Organizations.jsx
+++ src/Pages/Home/Organizations.jsx
@@ -18,6 +18,9 @@ const images = [
   "/Images/org/org-14.png",
   "/Images/org/org-15.png",
   "/Images/org/org-16.png",
+  "/Images/org/org-19.png",
+  "/Images/org/org-20.png",
+  "/Images/org/org-21.png",
 ]
 
 function Organizations() {
