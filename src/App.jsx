4f9bfdf06aef19329e68f68f66b4b82200716061
afreediz <afreedisulfiker@gmail.com>
2024-06-23 13:34:14 +0530

    removed class to className

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,9 @@ function App() {
 
   if (isMaintenance) {
     return(
-      <div class="flex flex-col justify-center items-center h-screen">
-        <h1 class="text-4xl font-bold text-white mb-4">We'll be back soon!</h1>
-        <p class="text-lg text-slate-100">Our site is currently undergoing scheduled maintenance. Thank you for your patience.</p>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold text-white mb-4">We'll be back soon!</h1>
+        <p className="text-lg text-slate-100">Our site is currently undergoing scheduled maintenance. Thank you for your patience.</p>
       </div>
     )
   }
